d619e51b8c1503e08933416ba7449a49ecee5ab7
jackcai1206 <jackcai1206@gmail.com>
2025-04-25 22:06:20 +0000

    fix: update parameters in generate_symbolic.sh for min-op, max-op, and examples-per-op

--- gsm-infinite/data/symbolic/generate_symbolic.sh
+++ gsm-infinite/data/symbolic/generate_symbolic.sh
@@ -3,7 +3,7 @@
 dataset_base_name=jackcai1206/gsm_infinite_symbolic
 
 # feel free to adjust the parameters.
-python3 generate_symbolic.py --dataset-name $dataset_base_name --length 0 --stride 1 --min-op 42 --max-op 120 --examples-per-op 2500
+python3 generate_symbolic.py --dataset-name $dataset_base_name --length 0 --stride 1 --min-op 1 --max-op 50 --examples-per-op 5000
 
 # python3 generate_symbolic.py --dataset-name $dataset_base_name --length 8k --stride 1 --min-op 1 --max-op 30 --examples-per-op 100
 
